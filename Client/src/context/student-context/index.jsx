378c773181559e999fd7c2c6e3f62a2e2cd9e16d
Hammad Bawany <hammadcreativedesigne89@gmail.com>
2024-12-25 23:58:44 +0500

    Course Details Added

--- Client/src/context/student-context/index.jsx
+++ Client/src/context/student-context/index.jsx
@@ -9,9 +9,13 @@ import { createContext, useState } from "react";
         const [studentCoursesList,setStudentCoursesList]=useState([])
 
 const [loadingState, setLoadingState] = useState(true)
-
+const [studentViewCourseDetails,setStudentViewCourseDetails] = useState(null)
+const [currentCourseDetailsId, setCurrentCourseDetailsId] = useState(null)
 
         return <StudentContext.Provider value={{studentCoursesList,setStudentCoursesList,
-            loadingState, setLoadingState
+            loadingState, setLoadingState,
+            studentViewCourseDetails,setStudentViewCourseDetails,
+            currentCourseDetailsId, setCurrentCourseDetailsId,
+
         }} >{children}</StudentContext.Provider>
     }
